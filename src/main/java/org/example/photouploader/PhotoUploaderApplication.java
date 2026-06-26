933af020fe7f92a54961dd083144053fb3ae26ca
Emmanuel Peprah Mensah <e.mensah@amalitech.com>
2026-06-26 10:01:11 +0000

    feat: initial Spring Boot project setup

---
+++ src/main/java/org/example/photouploader/PhotoUploaderApplication.java
@@ -0,0 +1,13 @@
+package org.example.photouploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoUploaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/photouploader/PhotoUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.photouploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoUploaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
